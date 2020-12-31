cb5b97993fec9ece06cc88c1277d4fadd83abfe4
sammyonyekwere <saamzinc@gmail.com>
2020-12-31 18:53:01 +0100

    initial commit

---
+++ contracts/Scoin.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+
+contract Scoin {
+    uint256 public totalSupply;
+    constructor() public {
+        totalSupply = 1000000;
+    }
+}
